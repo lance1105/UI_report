be568142f53c3123a1bcdec408c19c9392d4b649
Mark Jan Jacobi <mark@jacobi.pm>
2017-06-06 11:20:26 +0200

    Added resize script that uses convert

---
+++ img/resize.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+limits=(100 200 250 500 1000)
+foldername=resize
+rm -r $foldername;
+mkdir $foldername;
+for size in "${limits[@]}"; do
+    mkdir $foldername/w$size
+    find . -maxdepth 1 -regextype posix-extended -iregex ".*(png|jpg|jpeg)"| while read imgfile; do
+        convert $imgfile -resize $size $foldername/w$size/$imgfile
+        echo "Processing file '$imgfile' with '$size'"
+
+    done
+done
