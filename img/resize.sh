7b60943d2fe0eec938d75851746800c6b90e2e60
Mark Jan Jacobi <mark@jacobi.pm>
2017-06-11 22:48:47 +0200

    Added new image of adruino scematics and added new resolution

--- img/resize.sh
+++ img/resize.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-limits=(100 200 250 500 1000)
+limits=(100 200 250 500 750 1000)
 foldername=resize
 rm -r $foldername;
 mkdir $foldername;
 for size in "${limits[@]}"; do
     mkdir $foldername/w$size
     find . -maxdepth 1 -regextype posix-extended -iregex ".*(png|jpg|jpeg)"| while read imgfile; do
-        convert $imgfile -resize $size $foldername/w$size/$imgfile
+        convert "$imgfile" -resize $size "$foldername/w$size/$imgfile"
         echo "Processing file '$imgfile' with '$size'"
 
     done
